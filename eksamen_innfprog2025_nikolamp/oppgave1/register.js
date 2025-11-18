c50b19605a06556aad3d792ab0ac7d08735dd56f
Nikola714 <nikolamp@hiof.no>
2025-11-18 10:50:10 +0100

    metode med if-testen
    jeg brukte if-test for å legge tilpassende stilling

---
+++ eksamen_innfprog2025_nikolamp/oppgave1/register.js
@@ -0,0 +1,16 @@
+const ansatt = [
+    {
+        stilling: ["Professor", "Dekan", "Rektor", "Vaktmester", "Lektor"]
+    },
+    {
+        navn: "Tore Marius Akerbæk",
+        kontor: "D1-016",
+        epost: "tore.m.akerbak@hiof.no",
+        kursansvar: "Programmering1, Webutvikling",
+
+    }
+]
+
+if(navn === "Tore Marius Akerbæk"){
+    stilling += strilling[4]
+}
