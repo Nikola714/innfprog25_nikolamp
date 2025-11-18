7b438d92a0f6f73a91150b0a64a57cb489a4d1f5
Nikola714 <nikolamp@hiof.no>
2025-11-18 09:58:07 +0100

    Legge til forklaring
    Legget til kommentarer som forklarer python kode.

--- eksamen_innfprog2025_nikolamp/oppgave2/deloppgave1/oppgave2_1.py
+++ eksamen_innfprog2025_nikolamp/oppgave2/deloppgave1/oppgave2_1.py
@@ -1,4 +1,4 @@
-#1
+#1 lage array med indexer
 prw = [
     "kompilator",
     "variabel",
@@ -12,19 +12,23 @@ prw = [
     "kommentar"
 ]
 
-#2
+#2 definerer funkjson "lungo" som tar "ordliste" som en parameter.
+# if test sjekker om den paramet er definert, hvis ikke stop funkjson, 
+# hvis er definert funksjon gir den element som er lengst
 def lungo(ordliste):
-    if not ordliste:
+    if not ordliste: 
         return None
     return max(ordliste, key=len)
 
-#3
+#3 definere funksjon som tar imot et paramet
+#  funksjon legger til et ny element til array ved å bruke paramet og sletter mellomrom
 def add(nytt_ord):
     prw.append(nytt_ord.strip())
 
-#4
+#4 kalle funskjon som tar imot objektorientert som et paramet dvs. ny element i array
 add("objektorientert")
 
-#5
+#5 lage variabel som tar imot første funksjon som tar imot array som et paramet
+# i resultat for vi den lengste elemnt fra array 
 resultat = lungo(prw)
 print("Lengst:", resultat)
