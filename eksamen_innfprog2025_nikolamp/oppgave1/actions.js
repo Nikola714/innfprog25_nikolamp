d27baf8a22e32106e8495b71879920eff55aba51
Nikola714 <nikolamp@hiof.no>
2025-12-02 14:48:07 +0100

    skrive ut ansatte i liste

--- eksamen_innfprog2025_nikolamp/oppgave1/actions.js
+++ eksamen_innfprog2025_nikolamp/oppgave1/actions.js
@@ -3,22 +3,34 @@ import { ansatt } from './register.js';
 
 //Første funskjon til å indexere og skrive ut informasjon om ansatter
 function ansatteIndex(){
-    
+
+    const ansatter = document.getElementById("personer")
+
     //For løkke for å få indexer for hvert ansatt
-    for(let index = 0; index <= ansatt.length; index++){
-        console.log(index)
+    for(let index = 0; index < ansatt.length; index++){
+        ansatter.innerHTML += `
+        <li>
+            ${ansatt[index].navn}
+            <strong>Stilling: </strong>${ansatt[index].stilling}<br>
+            <strong>Kontor: </strong>${ansatt[index].kontor}<br>
+            <strong>E-post: </strong>${ansatt[index].epost}<br>
+            ${ansatt[index].kursansvar}
+            
+        </li>`
+
+        // console.log(index)
     }
     //Skrive ut datasett med alle ansatte
-    console.log(ansatt);
+    // console.log(ansatt);
 }
 //kjøre funksjon
 ansatteIndex()
 
 
-//Andre funksjon lister ut alle ansatte og bruke den første funksjon for å strukturere den html strukturen 
-function listerUt(){
-    //skrive ut informasjon for hvert ansatt som tilhører index
-    console.log(ansatt[index]);
+// //Andre funksjon lister ut alle ansatte og bruke den første funksjon for å strukturere den html strukturen 
+// function listerUt(){
+//     //skrive ut informasjon for hvert ansatt som tilhører index
+//     console.log(ansatt[index]);
 
-}
-listerUt()
+// }
+// listerUt()
