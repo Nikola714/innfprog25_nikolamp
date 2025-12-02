040e553af8b813570f5bfb6903680af079df033f
Nikola714 <nikolamp@hiof.no>
2025-12-02 09:17:38 +0100

    Første funksjon ferdig
    Første funskjon til å indexere og skrive ut informasjon om hvert ansatt

--- eksamen_innfprog2025_nikolamp/oppgave1/actions.js
+++ eksamen_innfprog2025_nikolamp/oppgave1/actions.js
@@ -1,8 +1,18 @@
 import { ansatt } from './register.js';
-console.log(ansatt);
 
-// function ansatteIndex (){
-//     const index = ansatt.indexOf("Lars-Petter Jelsness-Jørgensen")
-//     console.log(index)
-// }
-// ansatteIndex()
+
+//Første funskjon til å indexere og skrive ut informasjon om hvert ansatt
+function ansatteIndex(){
+    // const index = ansatt.indexOf("Lars-Petter Jelsness-Jørgensen")
+    // console.log(index)
+    
+    
+    //For løkke for å få indexer for hvert ansatt
+    for(let index = 0; index <= ansatt.length; index++){
+        console.log(index)
+        //skrive ut informasjon for hvert ansatt som tilhører index
+        console.log(ansatt[index]);
+    }
+}
+//kjøre funksjon
+ansatteIndex()
