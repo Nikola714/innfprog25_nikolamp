c384b4ab2aad5936a6a09b48d89e7da7e73bd000
Nikola714 <nikolamp@hiof.no>
2025-12-03 15:13:05 +0100

    fikk til funksjon 3
    fikk til funksjon som filterer etter stilling

--- eksamen_innfprog2025_nikolamp/oppgave1/actions.js
+++ eksamen_innfprog2025_nikolamp/oppgave1/actions.js
@@ -1,10 +1,10 @@
 import { ansatt } from './register.js';
 
+let ansatter = document.getElementById("personer")
 
 //Første funskjon til å indexere og skrive ut informasjon om ansatter
 function ansatteIndex(){
 
-    const ansatter = document.getElementById("personer")
 
     //For løkke for å få indexer for hvert ansatt
     for(let index = 0; index < ansatt.length; index++){
@@ -27,6 +27,81 @@ function ansatteIndex(){
 ansatteIndex()
 
 //Tredje funksjon tar imot en parameter som innsette stilling og filtrere alle ansatte på den innsette parameter, dvs. hvis noen velger dekan i meny kommer kun vare ansatte med stillinger dekan
+
+
+
+function visStilling(stilling){
+    ansatter.innerHTML= ""
+    ansatt.forEach(ansatt => {
+        if(ansatt.stilling === stilling){
+            ansatter.innerHTML += `
+                <li>
+                    ${ansatt.navn}
+                    <strong>Stilling: </strong>${ansatt.stilling}<br>
+                    <strong>Kontor: </strong>${ansatt.kontor}<br>
+                    <strong>E-post: </strong>${ansatt.epost}<br>
+                    ${ansatt.kursansvar}
+                    
+                </li>`
+        }
+    })
+}
+const btnProfessor = document.getElementById("professor");
+
+//Når noen klikker på knappen "Professor"...
+btnProfessor.addEventListener("click", () => {
+    //... får man liste bare med de ansatte med professor stilling
+    visStilling("Professor");
+});
+
+//Knappen for dekan
+const btnDekan = document.getElementById("dekan");
+
+btnDekan.addEventListener("click", () => {
+    visStilling("Dekan");
+});
+
+//Knappen for rektor
+const btnRektor = document.getElementById("rektor");
+
+btnRektor.addEventListener("click", () => {
+    visStilling("Rektor");
+});
+
+//Knappen for vaktmester
+const btnVaktmester = document.getElementById("vaktmester");
+
+btnVaktmester.addEventListener("click", () => {
+    visStilling("Vaktmester");
+});
+
+//Knappen for lektor
+const btnLektor = document.getElementById("lektor");
+
+btnLektor.addEventListener("click", () => {
+    visStilling("Lektor");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Tredje funksjon tar imot en parameter som innsette stilling og filtrere alle ansatte på den innsette parameter, dvs. hvis noen velger dekan i meny kommer kun vare ansatte med stillinger dekan
 // function filterStilling() {
 //     const professor = document.getElementById("professor")
 //     const professorFilter = ansatt.filter(
@@ -39,12 +114,12 @@ ansatteIndex()
 // }
 // filterStilling()  
 
-const btn = document.getElementById("professor")
-btn.addEventListener("click", printMsg)
+// const btn = document.getElementById("professor")
+// btn.addEventListener("click", printMsg)
 
-function printMsg() {
-    const professorStilling = ansatt.filter(
-        (person) => person.stilling === "Professor"
-    )
-    console.log(professorStilling)
-}
+// function printMsg() {
+//     const professorStilling = ansatt.filter(
+//         (person) => person.stilling === "Professor"
+//     )
+//     console.log(professorStilling)
+// }
